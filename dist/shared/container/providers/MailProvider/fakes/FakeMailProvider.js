3a5d143836af467b7ea83bc0821a89d6365af680
Rodrigo Engelberg <rodrigo.engelberg@tse.jus.br>
2021-03-15 09:58:21 -0300

    Ajustes projeto para deploy Heroku

---
+++ dist/shared/container/providers/MailProvider/fakes/FakeMailProvider.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+class FakeMailProvider {
+  messages = [];
+
+  async sendMail(message) {
+    this.messages.push(message);
+  }
+
+}
+
+exports.default = FakeMailProvider;
---
+++ dist/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+class FakeStorageProvider {
+  storage = [];
+
+  async saveFile(file) {
+    this.storage.push(file);
+    return file;
+  }
+
+  async deleteFile(file) {
+    const findIndex = this.storage.findIndex(storageFile => storageFile === file);
+    this.storage.splice(findIndex, 1);
+  }
+
+}
+
+var _default = FakeStorageProvider;
+exports.default = _default;
